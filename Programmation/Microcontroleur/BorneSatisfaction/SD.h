458161785d6acc6c2c66b340fe10827777dca442
Alex <peretjatko@univ-brest.fr>
2022-03-07 16:10:49 +0100

    Accès aux fichiers de la carte SD

--- Programmation/Microcontroleur/BorneSatisfaction/SD.h
+++ Programmation/Microcontroleur/BorneSatisfaction/SD.h
@@ -1,6 +1,6 @@
-#include "FS.h"
-#include "SD.h"
-#include "SPI.h"
+#include "libs/FS/src/FS.h";
+#include "libs/SD/src/SD.h";
+#include "libs/SPI/src/SPI.h";
 
 
 /**
@@ -8,8 +8,6 @@
    Ajout d'une ligne à la fin du fichier.
   ---------------------------------------------------------------------------------- */
 void SD_appendFile(fs::FS &fs, const char * path, const char * message) {
-  DEBUG("Appending to file: %s\n", path);
-
   File file = fs.open(path, FILE_APPEND);
   if (!file) {
     DEBUG("Failed to open file for appending");
@@ -21,4 +19,40 @@ void SD_appendFile(fs::FS &fs, const char * path, const char * message) {
     DEBUG("Append failed");
   }
   file.close();
+}
+
+
+/**
+   ----------------------------------------------------------------------------------
+   Ajout d'une ligne à la fin du fichier.
+   Ex : SD_readFile(SD, "/hello.txt")
+  ---------------------------------------------------------------------------------- */
+void SD_readFile(fs::FS &fs, const char * path){
+  File file = fs.open(path);
+  if(!file){
+    DEBUG("Failed to open file for reading");
+    return;
+  }
+
+  DEBUG("Read from file: ");
+  while(file.available()){
+    Serial.write(file.read());
+  }
+  file.close();
+}
+
+
+/**
+   ----------------------------------------------------------------------------------
+   test l'existance d'un fichier.
+   Ex : SD_existeFile(SD, "/hello.txt")
+  ---------------------------------------------------------------------------------- */
+boolean SD_existeFile(fs::FS &fs, const char * path){
+  File file = fs.open(path);
+  if(!file){
+    DEBUG("Failed to open file for reading");
+    return false;
+  } else {
+    return true;
+  }
 }
