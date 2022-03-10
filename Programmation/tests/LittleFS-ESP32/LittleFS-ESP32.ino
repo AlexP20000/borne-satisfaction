316082d51e20ab0e3fb78f4cfbf433dd140663f7
Alex <peretjatko@univ-brest.fr>
2022-03-10 19:26:13 +0100

    Test : LittleFS

--- Programmation/tests/LittleFS-ESP32/LittleFS-ESP32.ino
+++ Programmation/tests/LittleFS-ESP32/LittleFS-ESP32.ino
@@ -9,8 +9,6 @@
    If you test two partitions, you need to use a custom
    partition.csv file, see in the sketch folder */
 
-#define TWOPART
-
 #define FORMAT_LITTLEFS_IF_FAILED true
 
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels) {
@@ -243,17 +241,6 @@ void testFileIO(fs::FS &fs, const char * path) {
 void setup() {
   Serial.begin(115200);
 
-#ifdef TWOPART
-  if (!LITTLEFS.begin(FORMAT_LITTLEFS_IF_FAILED, "/lfs2", 5, "part2")) {
-    Serial.println("part2 Mount Failed");
-    return;
-  }
-  appendFile(LITTLEFS, "/hello0.txt", "World0!\r\n");
-  readFile(LITTLEFS, "/hello0.txt");
-  LITTLEFS.end();
-
-  Serial.println( "Done with part2, work with the first lfs partition..." );
-#endif
 
   if (!LITTLEFS.begin(FORMAT_LITTLEFS_IF_FAILED)) {
     Serial.println("LITTLEFS Mount Failed");
